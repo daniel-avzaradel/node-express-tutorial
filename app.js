1d49bb7f23537ca72b38564e9b6ea4b3f5bcb73c
Daniel Avzaradel <daniela@cgen.com>
2022-05-28 21:31:47 +0300

    mind grenade

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const names = require('./names');
 const sayHi = require('./utils');
+const data = require('./alternative');
+require('./mind-grenade');
 
 console.log(names);
 console.log(sayHi);
+console.log(data);
+console.log(data.items, data.singlePerson);
 
 sayHi(names.daniel);
 sayHi(names.john);
