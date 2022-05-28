bdd79f9a5cb5bd50870ac0f2d841ed22353a973e
Daniel Avzaradel <daniela@cgen.com>
2022-05-29 02:40:10 +0300

    fixing errors with http if-else

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
     res.end('Welcome to our server');
-  }
-  if (req.url === '/about') {
+  } else if (req.url === '/about') {
     res.end(`here's our short history`);
-  }
-  res.end(
-    `<h1>Oops!</h1>
+  } else {
+    res.end(
+      `<h1>Oops!</h1>
     <p>We cannot find the page you are looking for</p>
     <a href='/'>back to the home page</a>`,
-  );
+    );
+  }
 });
 
 server.listen(5000);
