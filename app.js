952a8fa5ca5fcc7b19c3bda8e3c13a08b4a33bec
Daniel Avzaradel <daniela@cgen.com>
2022-05-29 01:44:00 +0300

    sending errors on http requests

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.write('Welcome to our server');
-  res.end();
+  if (req.url === '/') {
+    res.end('Welcome to our server');
+  }
+  if (req.url === '/about') {
+    res.end(`here's our short history`);
+  }
+  res.end(
+    `<h1>Oops!</h1>
+    <p>We cannot find the page you are looking for</p>
+    <a href='/'>back to the home page</a>`,
+  );
 });
 
 server.listen(5000);
