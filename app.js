71f6a138bc2eb3d5979530aa3b331cd520e780a5
Daniel Avzaradel <daniela@cgen.com>
2022-05-29 14:24:47 +0300

    creating different url checks

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const http = require('http');
+
 const server = http.createServer((req, res) => {
-  res.end('Server online');
+  console.log(req.url);
+  if (req.url == '/') {
+    res.writeHead(200, {
+      'content-type': 'text/html',
+    });
+    res.write('<h1>Server online</h1>');
+    res.end();
+  } else if (req.url === '/about') {
+    res.writeHead(200, {
+      'content-type': 'text/html',
+    });
+    res.write('<h1>About page</h1>');
+    res.end();
+  } else {
+    res.writeHead(404, {
+      'content-type': 'text/html',
+    });
+    res.write('<h1>Page not found</h1>');
+    res.end();
+  }
 });
 
 server.listen(5000, () => {
