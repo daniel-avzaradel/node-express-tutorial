d58d521f85f9338befad865d897506dfa144a136
Daniel Avzaradel <daniela@cgen.com>
2022-05-29 09:33:23 +0300

    understanding event loops

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const _ = require('lodash');
+const http = require('http');
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('Welcome to our server');
+  } else if (req.url === '/about') {
+    // BLOCKING CODE !!!
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 999; j++) {
+        console.log(`${j}, ${i}`);
+      }
+    }
+    res.end('About page');
+  } else {
+    res.end('404, page not found');
+  }
+});
 
-console.log(newItems);
-console.log('Hello World');
+server.listen(5000, () => {
+  console.log(`Server is running on port 5000...`);
+});
