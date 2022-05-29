bd692d2456d724de367b2eded420e12e0b04e2a0
Daniel Avzaradel <daniela@cgen.com>
2022-05-29 13:12:10 +0300

    starting the express tutorial

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
 const http = require('http');
-
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.end('Welcome to our server');
-  } else if (req.url === '/about') {
-    // BLOCKING CODE !!!
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 999; j++) {
-        console.log(`${j}, ${i}`);
-      }
-    }
-    res.end('About page');
-  } else {
-    res.end('404, page not found');
-  }
+  res.end('Server online');
 });
 
 server.listen(5000, () => {
-  console.log(`Server is running on port 5000...`);
+  console.log(`Express tutorial. Server is running on port 5000...`);
 });
