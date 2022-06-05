7263d7dd824809aeff6c489d2e8ed07358632cab
Daniel Avzaradel <daniela@cgen.com>
2022-06-06 00:03:34 +0300

    absolute path method

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ console.log(sep);
 
 const filePath = path.join('/content', '/subfolder', 'test.txt');
 console.log(filePath);
+
+const base = path.basename(filePath);
+console.log(base);
+
+// path.resolve
+const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt');
+console.log(absolute);
