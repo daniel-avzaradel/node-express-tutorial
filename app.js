61bd39156fdfd7dcb14a36b3a6d8015ad26bfce5
Daniel Avzaradel <daniela@cgen.com>
2022-06-06 06:24:57 +0300

    creating routes with req.params

--- app.js
+++ app.js
@@ -18,7 +18,16 @@ app.get('/api/products/:productID', (req, res) => {
   console.log(req.params);
   const { productID } = req.params;
   const singleProduct = products.find((product) => product.id.toString() === productID);
+
+  if (!singleProduct) {
+    return res.status(404).send('Product not found! <a href="/api/products">products</a>');
+  }
   res.json(singleProduct);
+});
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+  console.log(req.params);
+  res.send('Hello World');
 });
 
 app.listen(5000, () => {
